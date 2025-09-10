1adcb677f37e858b48588ca3af07440e13d9e5b3
Chhavi123423 <chhavic612@gmail.com>
2025-09-10 00:27:07 +0000

    conversionofDatatypes

---
+++ 01_basics/ConversionOperation.js
@@ -0,0 +1,16 @@
+let score = 33;
+
+console.log(typeof (score));
+
+let IsValue = Number(score);
+console.log(typeof IsValue);
+
+let IsLoggedIn = "hitesh";
+console.log(typeof (IsLoggedIn));
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
