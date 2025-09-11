1382191af0a608c0a5de0ae0f3f67e6abfbe2ad0
Chhavi123423 <chhavic612@gmail.com>
2025-09-11 13:31:42 +0000

    Updated

--- 01_basics/ConversionOperation.js
+++ 01_basics/ConversionOperation.js
@@ -14,3 +14,20 @@ let stringNumber = String(someNumber);
 console.log(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//operaton
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2**3);
+
+let str1 = "hey";
+let str2 = " chhavi";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log(+true);
+
+console.log("02" > 1);
