fab934f544b12cae6b544fc8798a407fa3be0ede
Chhavi123423 <chhavic612@gmail.com>
2025-09-14 18:06:31 +0000

    updated

---
+++ 01_basics/objects.js
@@ -0,0 +1,22 @@
+//object literal
+
+const mySym = Symbol("key1");
+
+const jsUser = {
+    name: "nitya",
+    age: "18",
+    [mySym]: "key1",
+    location: "udaipur",
+    email: "nitya78@gmail.com",
+    isLoggedIn: false,
+    LastLoggedIn: ["Sunday", "Saturday"]
+}
+console.log(jsUser.name);
+console.log(jsUser["email"]);
+console.log(jsUser[mySym]);
+console.log(typeof jsUser.mySym);
+
+jsUser.name = "Neha";
+Object.freeze(jsUser);
+jsUser.name = "Sneha";
+console.log(jsUser);
