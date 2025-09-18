7eae72c5039f990b75d1855e16b7ba2ef697e91a
Chhavi123423 <chhavic612@gmail.com>
2025-09-18 14:16:52 +0000

    objects

---
+++ 01_basics/object_03.js
@@ -0,0 +1,24 @@
+//OBJECT DE-STRUCTURE
+
+const course = {
+    courseman: "Js",
+    price: "999",
+    courseInstructor: "Chhavi"
+}
+
+//course.ourseInstructor
+const {courseInstructor: Instructor} = course;
+
+console.log(Instructor);
+
+
+//JSON FORMAT
+// {
+//     "course" : "Chhvavi",
+//     "courseman" : "Javascript",
+//     "price" : "free"
+// }
+
+// [
+//     ARRAY IN JJSON FORM TOO
+// ]
